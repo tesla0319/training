04cb2558c6288d472763572d63577ad2e1106be1
Kimura <Ryunosuke_Kimura@totec.co.jp>
2023-05-29 10:12:46 +0900

    no message

---
+++ test.py
@@ -0,0 +1,10 @@
+import random
+# def randomhourse():
+#     r = random(1, 18)
+#     return r
+
+h1 = random(1, 18)
+h2 = random(1, 18)
+h3 = random(1, 18)
+
+print(f'オススメの三連単は{h1}, {h2}, {h3}です。')
